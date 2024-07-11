3bb99cd75b1f889c3f9d73f641ac40bc08b0d9c7
alexander shen <alexander.shen@gmail.com>
2024-07-11 13:42:40 -0400

    added basic links and information to header and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,18 @@ export default function Footer() {
   return (
     <footer>
       <p>Footer</p>
+      <a href="/" className="hover:underline">Home</a>
+      <a href="/about" className="hover:underline">About</a>
+      <a href="/games" className="hover:underline">Game Collection</a>
+      <a href="/menu" className="hover:underline">Menu</a>
+      <a href="/events" className="hover:underline">Events</a>
+      <a href="/reservations" className="hover:underline">Reservations</a>
+
+      <h3>Contact Us</h3>
+      <div className="contact-info">
+        <a href="tel:718-971-1684">(718) 971-1684</a>
+        <a href="mailto:sipnplaynyc@gmail.com">sipnplaynyc@gmail.com</a>
+      </div>
     </footer>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,14 @@ export default function Header() {
   return (
     <header>
       <p>Header</p>
+      <nav className="space-x-4">
+        <a href="/" className="hover:underline">Home</a>
+        <a href="/about" className="hover:underline">About</a>
+        <a href="/games" className="hover:underline">Game Collection</a>
+        <a href="/menu" className="hover:underline">Menu</a>
+        <a href="/events" className="hover:underline">Events</a>
+        <a href="/reservations" className="hover:underline">Reservations</a>
+      </nav>
     </header>
   );
 }
