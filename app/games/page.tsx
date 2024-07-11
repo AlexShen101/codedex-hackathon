d229afe57f2609ce4128ef0b5cf668224376d9f5
Julian Atkin <jatkindev@gmail.com>
2024-07-11 13:56:46 -0400

    Add API routes

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,5 +1,6 @@
-export default function Games() {
-  return (
-    <h1>Game page</h1>
-  );
+export default async function Games() {
+  const res = await fetch("http://127.0.0.1:3000/api/menu-items");
+  const data = await res.json();
+
+  return <h1>Game page</h1>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home() {
+export default async function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1>Home page</h1>
+    </>
   );
 }
