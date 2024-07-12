1781d40399d1af7e2b052bf39e932a6f065dca40
Julian Atkin <jatkindev@gmail.com>
2024-07-12 01:03:43 -0400

    Fix merge conflict

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
 
 // pages/events/[slug].js
 export default async function EventPage({
@@ -86,43 +85,10 @@ export default async function EventPage({
               <div className="bg-black w-3 h-3 rounded-full mr-2"></div>
               <span className="text-black">10 days to event</span>
             </div>
-<<<<<<< HEAD
-            <div className="desktop-container">
-                <div className="grid grid-cols-2 gap-4 w-fit mt-16 mb-16">
-                    <div className="bg-white p-8 rounded-lg shadow-custom h-fit">
-                        <h1 className="text-4xl font-bold mb-8">{event.title}</h1>
-                        <Link href={event.squareUrl} className="bg-black text-2xl text-green py-2 px-4 rounded-lg font-semibold w-full inline-block text-center shadow-custom">Register for Event</Link>
-                        <h1 className="text-2xl font-bold mb-2 mt-16">Event Description</h1>
-                        <p className="whitespace-pre-wrap">{event.description}</p>
-                    </div>
-                    <div className="bg-black text-white p-8 rounded-xl shadow-custom ml-8 h-fit">
-                        <div className="flex items-center mb-4 bg-green py-1 px-3 rounded-full w-fit">
-                            <div className="bg-black w-3 h-3 rounded-full mr-2"></div>
-                            <span className="text-black">10 days to event</span>
-                        </div>
-                        <div className="mb-4">
-                            <h3 className="text-lg font-bold">Date & Time</h3>
-                            <p>Fri, Jun 7</p>
-                            <p>6:00 PM - 11:00 PM EDT</p>
-                        </div>
-                        <div className="mb-4">
-                            <h3 className="text-lg font-bold">Location</h3>
-                            <p>471 5th Ave NY 11215</p>
-                        </div>
-                        <hr className="border-green mb-4" />
-                        <div className="flex flex-wrap">
-                            <div className="bg-white text-black py-1 px-3 rounded-full mr-2 mb-2">Magic the Gathering</div>
-                            <div className="bg-white text-black py-1 px-3 rounded-full mr-2 mb-2">Card Game</div>
-                            <div className="bg-white text-black py-1 px-3 rounded-full mr-2 mb-2">Fantasy</div>
-                        </div>
-                    </div>
-                </div>
-=======
             <div className="mb-4">
               <h3 className="text-lg font-bold">Date & Time</h3>
               <p>Fri, Jun 7</p>
               <p>6:00 PM - 11:00 PM EDT</p>
->>>>>>> aaffd99e8d8667c269fc37808bedd1a39bc0b6c1
             </div>
             <div className="mb-4">
               <h3 className="text-lg font-bold">Location</h3>
