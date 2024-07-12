35a96548a8cba789a484d405ce64644ae8074302
alexander shen <alexander.shen@gmail.com>
2024-07-11 21:11:33 -0400

    added pagination to menu

--- app/menu/components/menuCarousel.tsx
+++ app/menu/components/menuCarousel.tsx
@@ -1,22 +1,40 @@
 import MenuItemBox from './menuItem';
 import { MenuItem as MenuItemType } from '../interface/interfaces';
+import { useState } from 'react';
 
 interface MenuCarouselProps {
   menuItems: MenuItemType[];
 }
 
 function MenuCarousel({ menuItems }: MenuCarouselProps) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextItems = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 4) % menuItems.length);
+  };
+
+  const prevItems = () => {
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex - 4;
+      return newIndex < 0 ? menuItems.length + newIndex : newIndex;
+    });
+  };
+
+  const displayedItems = menuItems.slice(currentIndex, currentIndex + 4).concat(
+    menuItems.slice(0, Math.max(0, (currentIndex + 4) - menuItems.length))
+  );
+
   return (
     <div className="flex justify-center items-center">
-      <button className="carousel-button bg-light-brown text-white rounded-full p-4 cursor-pointer mx-4">
+      <button onClick={prevItems} className="carousel-button bg-light-brown hover:bg-dark-brown text-white rounded-full p-4 cursor-pointer mx-4">
         <i className="fas fa-chevron-left"></i>
       </button>
       <div className="grid grid-cols-2 gap-4">
-        {menuItems.map((item, index) => (
-          <MenuItemBox key={index} {...item} />
+        {displayedItems.map((item, index) => (
+          <MenuItemBox key={item.id} {...item} />
         ))}
       </div>
-      <button className="carousel-button bg-light-brown text-white rounded-full p-4 cursor-pointer mx-4">
+      <button onClick={nextItems} className="carousel-button bg-light-brown hover:bg-dark-brown text-white rounded-full p-4 cursor-pointer mx-4">
         <i className="fas fa-chevron-right"></i>
       </button>
     </div>
