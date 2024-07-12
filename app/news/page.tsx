a4c9934ef841dd5a59e24c8ed72a299b93d93d7f
alexander shen <alexander.shen@gmail.com>
2024-07-12 08:15:05 -0400

    changed footer links to redirect to correct urls, added images to news url

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 
 export default function News() {
   return (
@@ -5,7 +6,7 @@ export default function News() {
       <h1 className="text-3xl font-bold mb-6">News</h1>
       <div className="space-y-6">
         <div className="flex border rounded-lg overflow-hidden shadow-lg">
-          <img src="https://placehold.co/300x200" alt="Hands playing a board game" className="w-1/3 object-cover" />
+          <Image src='/patch article.webp' width={674} height={653} alt="People playing a board game" className="w-1/3 object-cover" />
           <div className="p-4 w-2/3 bg-white">
             <h2 className="text-xl font-semibold mb-2">‘Board Game Cafe’ Opens on Park Slope’s Fifth Avenue</h2>
             <p className="text-gray-600 mb-4">January 2, 2020</p>
@@ -13,7 +14,7 @@ export default function News() {
           </div>
         </div>
         <div className="flex border rounded-lg overflow-hidden shadow-lg">
-          <img src="https://placehold.co/300x200" alt="Person standing in a board game cafe" className="w-1/3 object-cover" />
+          <Image src='/cafe_image.webp' width={726} height={572} alt="Image of the cafe window" className="w-1/3 object-cover" />
           <div className="p-4 w-2/3 bg-white">
             <h2 className="text-xl font-semibold mb-2">Board Game Cafe Opens in Park Slope</h2>
             <p className="text-gray-600 mb-4">January 6, 2020</p>
