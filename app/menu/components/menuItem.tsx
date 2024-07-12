e5578303fd0d3c570a892eb373381333e6082602
alexander shen <alexander.shen@gmail.com>
2024-07-11 21:33:45 -0400

    incomplete styling for menu items

--- app/menu/components/menuItem.tsx
+++ app/menu/components/menuItem.tsx
@@ -4,18 +4,31 @@ import { MenuItem } from '../interface/interfaces'
 export default function MenuItemBox({ id, attributes }: MenuItem) {
   const { name, description, menu_item_prices } = attributes;
   const altText = `${name} image`;
-  
+
   // Extracting prices from menu_item_prices
-  const prices = menu_item_prices.data.map(price => price.attributes.price);
+  const price_data = menu_item_prices.data.map(price => {
+    return {
+      price: price.attributes.price,
+      label: price.attributes.label
+    }
+  });
 
   return (
-    <div className="menu-item bg-black text-white rounded-md p-4 m-4 w-[300px]">
-      <img src={""} alt={altText} className="w-12 h-12" />
-      <h3 className="text-xl font-bold">{name}</h3>
-      <p>{description}</p>
-      {prices.map((price, index) => (
-        <p key={index}>{price}</p>
-      ))}
+    <div className="menu-item bg-black text-white rounded-xl p-4 m-4 flex items-center space-x-8 w-[500px]">
+      <img src={""} alt={altText} className="w-24 h-24" />
+      <div>
+        <h1 className="text-3xl font-bold">{name}</h1>
+        <div className="border-t-2 border-green w-16 my-2"></div>
+        <p className="text-sm mb-4">{description}</p>
+      </div>
+      <div className="flex space-x-8">
+        {price_data.map((price_item, index) => (
+          <>
+            <p className="font-bold" key={index}>{price_item.label}</p>
+            <p key={index}>{price_item.price}</p>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
