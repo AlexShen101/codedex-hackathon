76fb17909a1ee13fcd04c50cb7c532755d9a0785
alexander shen <alexander.shen@gmail.com>
2024-07-12 03:52:17 -0400

    added events display to main page

---
+++ components/eventDisplay.tsx
@@ -0,0 +1,56 @@
+import EventCard from "@/app/events/components/eventCard"
+
+export default async function EventDisplay() {
+  const res = await fetch(`${process.env.STRAPI_API_URL}/events?populate=*`, {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_API_SECRET}`,
+    },
+  });
+  const data = await res.json();
+
+  let events = [];
+
+  for (const dataEvent of data.data) {
+    let attributes = dataEvent.attributes;
+    let timeSlots = attributes.event_time_slots.data;
+
+    let coverImage = null;
+
+    if (attributes.coverimage.data) {
+      coverImage = {
+        width: attributes.coverimage.data.attributes.width,
+        height: attributes.coverimage.data.attributes.height,
+        url: attributes.coverimage.data.attributes.url,
+      };
+    }
+
+    for (const timeSlot of timeSlots) {
+      let event = {
+        title: attributes.title,
+        description: attributes.description,
+        coverImage: coverImage,
+        date: new Date(timeSlot.attributes.datetime),
+        squareUrl: timeSlot.attributes.squareurl,
+      };
+
+      events.push(event);
+    }
+  }
+
+  // sort events by date
+  events.sort((a, b) => a.date.getTime() - b.date.getTime())
+
+  const today = new Date();
+  // Filter events to only include those after today, and then slice the 3 closest ones
+  const upcomingEvents = events.filter((event) => event.date >= today).slice(0, 3);
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        {upcomingEvents.map((event, index) => (
+          <EventCard key={index} event={event} />
+        ))}
+      </div>
+    </>
+  )
+}
---
+++ components/homeEventsSection.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import EventDisplay from './eventDisplay'
+
+export default function HomeEventsSection() {
+  return (
+    <>
+      <section className="desktop-container">
+        <div className="flex justify-between items-baseline mb-4">
+          <h1 className="font-semibold text-3xl">Exciting Events</h1>
+          <Link className="text-light-brown font-semibol transition hover:underline" href="/events">See All</Link>
+        </div>
+        <EventDisplay />
+      </section>
+    </>
+  )
+}
