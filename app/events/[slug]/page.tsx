4c4f850463559e8c1235d98ada5348d48deaf1be
alexander shen <alexander.shen@gmail.com>
2024-07-12 01:35:48 -0400

    added date display for event details page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -7,8 +7,7 @@ export default async function EventPage({
 }: {
   params: { slug: string };
 }) {
-  console.log(params);
-  const res = await fetch(`${process.env.STRAPI_API_URL}/events?populate=*`, {
+  const res = await fetch(`${process.env.STRAPI_API_URL}/event-time-slots?filters[squareurl][$eq]=https://square.link/u/${params.slug}&populate=event`, {
     headers: {
       Authorization: `Bearer ${process.env.STRAPI_API_SECRET}`,
     },
@@ -17,45 +16,43 @@ export default async function EventPage({
 
   let event = null;
 
-  for (const dataEvent of data.data) {
-    let attributes = dataEvent.attributes;
-    let timeSlots = attributes.event_time_slots.data;
+  let timeSlot = data.data ? data.data[0].attributes : null
 
-    let coverImage = null;
-
-    if (attributes.coverimage.data) {
-      coverImage = {
-        width: attributes.coverimage.data.attributes.width,
-        height: attributes.coverimage.data.attributes.height,
-        url: attributes.coverimage.data.attributes.url,
-      };
-    }
-
-    for (const timeSlot of timeSlots) {
-      let squareUrl = timeSlot.attributes.squareurl;
-
-      const regex = /https:\/\/square\.link\/u\/([a-zA-Z0-9]+)/;
-      const match = squareUrl.match(regex);
-
-      if (match) {
-        if (match[1] == params.slug) {
-          event = {
-            title: attributes.title,
-            description: attributes.description,
-            coverImage: coverImage,
-            date: new Date(timeSlot.attributes.datetime),
-            squareUrl: squareUrl,
-          };
-        }
-      }
-    }
+  if (!timeSlot) {
+    return <div>Square Link not found.</div>;
   }
 
-  if (!event) {
+  let eventDetails = timeSlot.event.data ? timeSlot.event.data.attributes : null
+
+  if (!eventDetails) {
     return <div>Event not found.</div>;
   }
 
-  console.log(event);
+  event = {
+    title: eventDetails.title,
+    description: eventDetails.description,
+    date: new Date(timeSlot.datetime),
+    squareUrl: timeSlot.squareurl,
+  }
+
+  const today = new Date()
+  const diffTime = event.date.getTime() - today.getTime()
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+
+  const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  const dayOfWeek = weekDays[event.date.getDay()]
+
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const month = monthNames[event.date.getMonth()]
+
+  let hours = event.date.getHours();
+  const minutes = event.date.getMinutes();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  // Convert to 12-hour format
+  hours = hours % 12 || 12;
+  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+  const formattedTime = `${hours}:${formattedMinutes} ${ampm}`;
 
   return (
     <div className="flex flex-col items-center">
@@ -84,12 +81,12 @@ export default async function EventPage({
           <div className="bg-black text-white p-8 rounded-xl shadow-custom ml-8 h-fit">
             <div className="flex items-center mb-4 bg-green py-1 px-3 rounded-full w-fit">
               <div className="bg-black w-3 h-3 rounded-full mr-2"></div>
-              <span className="text-black">10 days to event</span>
+              <span className="text-black">{diffDays < 0 ? "Event has passed." : `${diffDays} days till the event.`}</span>
             </div>
             <div className="mb-4">
               <h3 className="text-lg font-bold">Date & Time</h3>
-              <p>Fri, Jun 7</p>
-              <p>6:00 PM - 11:00 PM EDT</p>
+              <p>{dayOfWeek}, {month} {event.date.getDay()}</p>
+              <p>{formattedTime} EDT</p>
             </div>
             <div className="mb-4">
               <h3 className="text-lg font-bold">Location</h3>
